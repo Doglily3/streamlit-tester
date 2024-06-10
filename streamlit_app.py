49fe7ef78546b04885ce7a8df35beb01a17d9750
judy <judyshi.peixin@gmail.com>
2024-06-10 23:39:26 +0800

    gpt version selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,16 @@ from langchain_community.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='default')
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+model_version = st.sidebar.selectbox('Select Model Version', ['gpt-3.5-turbo', 'gpt-4'])
 
 def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-    st.info(llm(input_text))
+    if openai_api_key.startswith('sk-'):
+        llm = OpenAI(model_name=model_version, temperature=0.7, openai_api_key=openai_api_key)
+        response = llm(input_text)
+        st.info(response)
+    else:
+        st.warning('Invalid OpenAI API key!', icon='⚠')
 
 with st.form('my_form'):
     text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
